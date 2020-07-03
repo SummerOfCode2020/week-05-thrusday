b42a19555be3519fd7570be7e5c79c63940dbabc
Johnny Rice <jrice@labact.com>
2020-07-02 20:41:26 -0400

    Drop one or more items from the end

--- dropRight.js
+++ dropRight.js
@@ -1,6 +1,18 @@
-function all(arr) {
-	// Loop through every item in the array
-	// If the item evaluates to false, return false
-
-	// If the loop ends, return true
+function dropRight(items, numberOfItemsToDrop) {
+	// if the array is shorter than the number do drop, 
+	// then we can return an empty array
+	if (numberOfItemsToDrop >= items.length) return []
+	
+	// subtract the number to drop from the number of items
+	// this gives us the number we are keeping
+	let itemsToKeep = (items.length - numberOfItemsToDrop);
+	
+	// start at the first item and return as many as we calculated
+	return items.slice(0, itemsToKeep)
 }
+
+const friends = ['Jim B.', 'Tito', 'Jack D.']
+const goodFriends = dropRight(friends, 1)
+const bestFriendForever = dropRight(friends, 2)
+
+console.log( {friends, goodFriends, bestFriendForever })
